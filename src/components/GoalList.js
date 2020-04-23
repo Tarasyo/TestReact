a658db1116d8ff9c815cc55da1981e6713248ebd
Taras Boreyko <tarasyo7@gmail.com>
2020-04-23 19:32:53 +0100

    use of first props and reading the list

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './GoalList.css';
 
-const GoalList = () => {
-    return (
-    <ul className="goal-list">
-      <li>Finish the Course</li>
-      <li>Lear all about the Course Main Topic</li>
-      <li>Help others student in the Course Q&amp;A</li>
-    </ul>
-    );
+const GoalList = props => {
+    return <ul className="goal-list">
+        {props.goals.map((goal) => {
+        return <li key={goal.id}>{goal.text}</li>
+        })}
+        </ul>;   
 };
 
 export default GoalList;

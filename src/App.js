52982480e187ff3beb743034d6082087feca8418
Taras Boreyko <tarasyo7@gmail.com>
2020-04-23 22:08:53 +0100

    Add New Goal function and state for array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import GoalList from './components/GoalList';
+import NewGoal from './components/NewGoal';
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     {id: 'cg1', text: 'Finish the Course'},
     {id: 'cg2', text: 'Lear all about the Course Main Topic'},
     {id: 'cg3', text: 'Help others student in the Course Q&A'},
-  ];
+  ]);
+
+  const addNewGoalHandler = (newGoal) => {
+    setCourseGoals(courseGoals.concat(newGoal));
+  };
 
   return <div className="course-goals">
     <h2>Course Goals</h2>
+    <NewGoal onAddGoal={addNewGoalHandler} />
     <GoalList goals={courseGoals}/>
   </div>;
 };
